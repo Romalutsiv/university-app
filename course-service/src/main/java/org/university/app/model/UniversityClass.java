a2c1f29c96f9ba3489806dd77175525f3233bfbd
Roma <lutsiv55@gmail.com>
2024-08-28 16:00:36 +0300

    Update models to documents.

--- course-service/src/main/java/org/university/app/model/UniversityClass.java
+++ course-service/src/main/java/org/university/app/model/UniversityClass.java
@@ -1,6 +1,8 @@
 package org.university.app.model;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
@@ -9,7 +11,9 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document
 public class UniversityClass {
+    @Id
     private String id;
     private int yearOfStudy;
     private String className;
