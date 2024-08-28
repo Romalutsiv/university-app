ea89b667c450564fbb870078bd7e009715a3ff06
Roma <lutsiv55@gmail.com>
2024-08-28 13:21:07 +0300

    Create user profile models.

---
+++ user-profile-service/src/main/java/org/university/app/model/DoctoralDegree.java
@@ -0,0 +1,5 @@
+package org.university.app.model;
+
+public enum DoctoralDegree {
+    POSTGRADUATE, DOCENT, PROFESSOR
+}
---
+++ user-profile-service/src/main/java/org/university/app/model/StudentProfile.java
@@ -0,0 +1,15 @@
+package org.university.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentProfile {
+    private int courseNumber;
+
+}
