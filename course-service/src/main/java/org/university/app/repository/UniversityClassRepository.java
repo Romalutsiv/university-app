bd78b518d6b41e7cacfbf8d8f82c9310243432c3
Roma <lutsiv55@gmail.com>
2024-08-28 16:58:31 +0300

    Add repository methods.

--- course-service/src/main/java/org/university/app/repository/UniversityClassRepository.java
+++ course-service/src/main/java/org/university/app/repository/UniversityClassRepository.java
@@ -1,6 +1,10 @@
 package org.university.app.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.university.app.model.UniversityClass;
+import reactor.core.publisher.Mono;
 
-public interface UniversityClassRepository extends ReactiveMongoRepository<UniversityClassRepository, String> {
+public interface UniversityClassRepository extends ReactiveMongoRepository<UniversityClass, String> {
+    Mono<UniversityClass> findByStudentIdsContaining(String studentId);
+    Mono<UniversityClass> findByClassTeacherId(String teacherId);
 }
